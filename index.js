bb7ba7327b3b60869b86800a137089009f842fcd
Godwin <godwin.yampoi@student.moringaschool.com>
2023-03-31 01:08:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(title => {
+    let words = title.split(' ');
+    let capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
 }
